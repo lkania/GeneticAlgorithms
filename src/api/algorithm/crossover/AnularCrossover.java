4978badc1b9bb7bb8ac24722aeff80ee7e69c3f9
f <f@f-VirtualBox.(none)>
2016-06-22 01:31:09 -0300

    fix anular crossover

--- src/api/algorithm/crossover/AnularCrossover.java
+++ src/api/algorithm/crossover/AnularCrossover.java
@@ -9,8 +9,8 @@ public class AnularCrossover extends TwoIndexCrossover{
 
     @Override
     public void crossover(Individual i1, Individual i2, List<Individual> ans) {
-        int firstIndex = (int) (RandomNumbers.getInstance().getRandomNumber()*ans.size());
-        int secondIndex = ((int) (RandomNumbers.getInstance().getRandomNumber()*ans.size()*0.5+1)+firstIndex)%ans.size();
+        int firstIndex = (int) (RandomNumbers.getInstance().getRandomNumber()*i1.getGens().size());
+        int secondIndex = ((int) (RandomNumbers.getInstance().getRandomNumber()*i1.getGens().size()*0.5+1)+firstIndex)%i1.getGens().size();
 
         crossover(i1,i2,ans,Math.min(firstIndex,secondIndex),Math.max(firstIndex,secondIndex));
     }
